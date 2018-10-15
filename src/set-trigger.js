62d8cf00282467cabc4465a93aedfbb53f982b95
Mark Stier <stier020@Marks-MacBook-Pro-2.local>
2018-10-14 18:55:02 -0700

    add logs

--- src/set-trigger.js
+++ src/set-trigger.js
@@ -3,10 +3,12 @@ var Process = require("./process");
 var fs = require("fs");
 var SHARED_STATE = require("./shared-state");
 var path = require("path");
+
 module.exports = (lambdaName, cronString, num) => {
   try{
     let {name, runFile, maxMemory, timeout} = JSON.parse(fs.readFileSync(path.join(__dirname, "../datastore", lambdaName + ".json"), "utf8"))
     var job = new CronJob(cronString, () => {
+      fs.appendFileSync(path.join(__dirname, "../../log.txt"), `Starting ${name}: ${cronString}`);
       try{
         for(var i = 0; i < parseInt(num); i++){
           var proc = new Process(name, runFile, timeout, maxMemory);
@@ -14,10 +16,11 @@ module.exports = (lambdaName, cronString, num) => {
           proc.run({time:Date.now()});
         }
       } catch(e){
+        fs.appendFileSync(path.join(__dirname, "../../error-log.txt"), e.stack.toString());
       }
     });
   } catch(e){
-
+    fs.appendFileSync(path.join(__dirname, "../../error-log.txt"), e.stack.toString());
   }
 
   SHARED_STATE.jobs.push(job);
